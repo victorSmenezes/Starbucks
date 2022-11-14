d8e5aa87371835e5d72e97a7437df0baf85f712c
Dusky7 <victorsantos12dima@gmail.com>
2022-11-14 15:00:37 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let img = document.querySelector('.starbucks')
+let circulo = document.querySelector('.circulo')
+
+function trocaImagem(endereco){
+   img.src = endereco
+}
+
+function trocaCor(cor){
+    circulo.style.background = cor
+}
+
